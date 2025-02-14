a002ff484ba21adb079461339d8295c3b9096a85
Shashank S <shashankshivu4@gmail.com>
2025-02-14 10:40:42 +0530

    css updates

--- src/Components/SkillItem/SkillItem.jsx
+++ src/Components/SkillItem/SkillItem.jsx
@@ -1,6 +1,7 @@
 import "./SkillItem.css"
+import {nanoid} from"nanoid"
 export default function SkillItem(props){
-    const tags=props.items.map((item)=>(<button className="skill-button">{item}</button>))
+    const tags=props.items.map((item)=>(<button key={nanoid()}className="skill-button">{item}</button>))
     return(
         <div className="category-container">
             <div className="title-card">
--- src/Components/Socials/Socials.jsx
+++ src/Components/Socials/Socials.jsx
@@ -7,7 +7,7 @@ export default function Socials(props){
             <div className="icons-display" >
                 <div className="elements" onClick={() => window.open("https://www.linkedin.com/in/shashank-s-391a58266/", "_blank")}><Linkedin size={32} className="lucide"/><span>Linkedin</span></div>
                 <div className="elements"onClick={() => window.open("https://www.instagram.com/prime_295/", "_blank")}><Instagram size={32} className="lucide"/><span>Instagram</span></div>
-                <div className="elements"onClick={() => window.open("mailto:someone@example.com", "_blank")}><Mail size={32} className="lucide"/><span>Email</span></div>
+                <div className="elements"onClick={() => window.open("mailto:shashankshivu4@gmail.com", "_blank")}><Mail size={32} className="lucide"/><span>Email</span></div>
                 <div className="elements"onClick={() => window.open("https://github.com/Sthor21", "_blank")}><Github size={32} className="lucide"/><span>Github</span></div>
                 <div className="elements"onClick={() => window.open("https://leetcode.com/u/Shashank_thor/", "_blank")}><ChevronRight size={32} className="lucide"/><span>Leetcode</span></div>
             </div>
